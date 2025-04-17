a9687cb3542dae576e076ffef009114e1c8b810d
Orozco Enzo Mariano <marianoorozco700@outlook.com>
2025-04-17 09:47:00 -0300

    Por las dudas

---
+++ codigos.py
@@ -0,0 +1,9 @@
+class Codigos:
+    codificado = {
+        "Adic. Ventas" : 45,
+        "Ant. de Sueldos" : 610,
+        "Feriado" : 8,
+        "Licencia" : 409,
+        "D. Enfermedad" : 65,
+        "D. Estudio" : 62
+    }
